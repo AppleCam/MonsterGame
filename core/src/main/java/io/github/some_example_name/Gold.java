88106fd97f9adcbfa7ffb432bc590e90d7fece35
Cameron Appleby <camappleby@gmail.com>
2025-02-11 10:57:46 +0000

    Impliment new levels and walls

---
+++ core/src/main/java/io/github/some_example_name/Gold.java
@@ -0,0 +1,112 @@
+package io.github.some_example_name;
+
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.utils.Array;
+
+import java.util.Iterator;
+import java.util.Random;
+
+public class Gold {
+    private Array<GoldCoin> coins; // Array to store multiple GoldCoin instances
+    private Array<Trap> traps; // Array to store multiple Trap instances
+    private ShapeRenderer shapeRenderer;
+    private Random random;
+    public int count = 0;
+    private int coinToWin = 100;
+
+
+    public Gold(int numberOfCoins, int gridSize) {
+        this.coins = new Array<>();
+        this.traps = new Array<>();
+        this.shapeRenderer = new ShapeRenderer();
+        this.random = new Random();
+
+        // Generate coins and traps at random positions in the grid
+        for (int i = 0; i < numberOfCoins; i++) {
+            int gridX = random.nextInt(gridSize); // Random grid X
+            int gridY = random.nextInt(gridSize); // Random grid Y
+            int coinX = gridX * 50 + 25; // X Position (center of grid box)
+            int coinY = gridY * 50 + 25; // Y Position (center of grid box)
+            coins.add(new GoldCoin(coinX, coinY, 12)); // Create a GoldCoin with a radius
+
+            // 10% chance to spawn a trap on the same square as a coin
+            if (random.nextDouble() < 0.1) {
+                traps.add(new Trap(coinX, coinY, 12)); // Create a Trap with the same position and radius
+            }
+        }
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public void draw() {
+        shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
+
+
+
+        shapeRenderer.setColor(0.15f, 0.15f, 0.2f, 1f);
+        //shapeRenderer.setColor(Color.WHITE);
+        for (Trap trap : traps) {
+            // Draw traps as circles (invisible in the game but noted for development)
+            trap.getBounds().setRadius(0); // Set radius to 0 for invisibility
+            shapeRenderer.circle(trap.getBounds().x, trap.getBounds().y, 12); // Draw trap as a circle
+        }
+
+        shapeRenderer.setColor(Color.GOLD); // Set color to gold for coins
+        for (GoldCoin coin : coins) {
+            coin.draw(shapeRenderer); // Draw each coin
+        }
+        shapeRenderer.end();
+    }
+
+    public void collectCoins(Player player) {
+        Iterator<GoldCoin> iterator = coins.iterator();
+        while (iterator.hasNext()) {
+            GoldCoin coin = iterator.next();
+            // Check if the player has collected this coin
+            if (player.getBounds().overlaps(coin.getBounds())) {
+                iterator.remove(); // Remove the coin from the array
+                System.out.println("Coin collected!"); // Notify that a coin has been collected
+                count = count +1;
+                System.out.println(count);
+            }
+        }
+    }
+
+    public void checkTraps(Player player) {
+        Iterator<Trap> iterator = traps.iterator();
+        while (iterator.hasNext()) {
+            Trap trap = iterator.next();
+            // Check if the player has stepped on the trap
+            if (player.getBounds().overlaps(trap.getBounds())) {
+                 // Reset the player if they step on a trap
+                //System.out.println("Trap activated! Monster is awake!!"); // Notify that a trap was activated
+                trap.activateTrap(player);
+
+            }
+        }
+    }
+
+    public void win(){
+        if (count == coinToWin) {
+            System.out.println("You win!");
+            System.exit(0);
+        }
+    }
+
+    public void dispose() {
+        shapeRenderer.dispose();
+        for (GoldCoin coin : coins) {
+            coin.dispose();
+        }
+        for (Trap trap : traps) {
+
+        }
+    }
+}
---
+++ core/src/main/java/io/github/some_example_name/Monster.java
@@ -0,0 +1,104 @@
+package io.github.some_example_name;
+
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.Circle;
+import com.badlogic.gdx.math.Vector2;
+
+public class Monster {
+    private ShapeRenderer shapeRenderer;
+    private Vector2 position; // Position of the monster
+    private int radius; // Radius of the monster
+    private float speed = 2.0f; // Speed at which the monster chases the player
+    private int x, y;
+    private int dx = 50, dy = 50; // Grid cell size
+    private boolean canMove = true; // Flag to control monster movement
+    private Circle MBounds;
+
+    public Monster(int x, int y, int radius) {
+        this.position = new Vector2(x, y); // Initialize monster position
+        this.radius = radius; // Set monster radius
+        this.shapeRenderer = new ShapeRenderer(); // Create ShapeRenderer for drawing
+        this.x = x;
+        this.y = y;
+        this.MBounds = new Circle(x, y, radius);
+    }
+
+    public void attack(Player player) {
+        if (player.getBounds().overlaps(getMBounds())){
+            System.exit(0);
+        }
+    }
+
+    public void move(Player player) {
+        // Check if the monster should chase the player
+        if (player.isChased() && canMove) {
+            // 1. Get the player's grid coordinates
+            int playerGridX = (int) (player.getX() / dx);
+            int playerGridY = (int) (player.getY() / dy);
+
+            // 2. Get the monster's grid coordinates
+            int monsterGridX = (int) (position.x / dx);
+            int monsterGridY = (int) (position.y / dy);
+
+            // 3. Determine the direction to move (up, down, left, right)
+            int directionX = playerGridX - monsterGridX;
+            int directionY = playerGridY - monsterGridY;
+
+            // 4. horizontal movement
+            if (Math.abs(directionX) >= Math.abs(directionY)) {
+                if (directionX > 0) {
+                    // Move right
+                    position.x += dx;
+                    canMove = false;
+                } else if (directionX < 0) {
+                    // Move left
+                    position.x -= dx;
+                    canMove = false;
+                }
+            } else { //vertical movement
+                if (directionY > 0) {
+                    // Move down
+                    position.y += dy;
+                    canMove = false;
+                } else if (directionY < 0) {
+                    // Move up
+                    position.y -= dy;
+                    canMove = false;
+                }
+            }
+
+            // Update monster's position
+            x = (int) position.x;
+            y = (int) position.y;
+            MBounds.setPosition(x, y);
+
+            // Prevent the monster from moving again until the player's turn
+            canMove = false;
+        }
+    }
+
+    // Method to allow the monster to move again
+    public void allowMovement() {
+        canMove = true;
+    }
+
+    public void draw() {
+        shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
+        shapeRenderer.setColor(Color.RED); // Set color for the monster
+        shapeRenderer.circle(position.x, position.y, radius); // Draw monster as a circle
+        shapeRenderer.end();
+    }
+
+    public void dispose() {
+        shapeRenderer.dispose(); // Dispose of ShapeRenderer resources
+    }
+
+    public Vector2 getPosition() {
+        return position; // Get current position of the monster
+    }
+
+    public Circle getMBounds() {
+        return MBounds;
+    }
+}
